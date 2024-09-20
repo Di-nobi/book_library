6107fb15160a412457689760d491a4ab72a19842
Dinobi <architectstan01@gmail.com>
2024-09-20 11:42:12 +0100

    Corrected the routes in pytest

--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -31,5 +31,40 @@ def test_get_unavailable_books(client):
 def test_get_users(client):
     response = client.get('/api/v1/get_users')
     assert response.status_code == 200
-    assert response.json == {'message': [{'firstName': 'Dinobi1', 'lastName': 'Udeh', 'email': 'udehdinobi@gmail.com'}]}
+    assert response.json == {'message': [{'firstName': 'Dinobi1', 'lastName': 'Udeh', 'email': 'udehdinobi@gmail.com'}, {'email': 'udehdinobi@gmail.com', 'firstName': 'Dinobi2', 'lastName': 'Udeh'}]}
 
+def test_get_unavailable_books(client):
+    response = client.get('/api/v1/unavailable_books')
+    assert response.status_code == 200
+    expected_books = [
+        {
+            "category": "Fiction",
+            "due_date": "2024-09-27",
+            "id": "2c7a06c7-0b71-407e-bd7a-224f4ae6bc1e",
+            "publisher": "HarperCollins",
+            "title": "The Alchemist of Dinobi"
+        },
+        {
+            "category": "Fiction",
+            "due_date": "2024-09-27",
+            "id": "b3a756b0-5db2-45df-bd55-f4e7d4e78e64",
+            "publisher": "HarperCollins",
+            "title": "The Alchemist of Dinobi22"
+        },
+        {
+            "category": "Fiction",
+            "due_date": "2024-09-27",
+            "id": "492c4766-9fa5-4ace-8a1b-25c49544f0d7",
+            "publisher": "HarperCollins",
+            "title": "The Alchemist of Dinobi23"
+        },
+        {
+            "category": "Fiction",
+            "due_date": "2024-09-27",
+            "id": "c334e869-80f7-4c0a-8ffb-dbe594dc11b7",
+            "publisher": "HarperCollins",
+            "title": "The Alchemist of Dinobi24"
+        }
+    ]
+    actual_books = response.json['unavailable books']
+    assert all(book in actual_books for book in expected_books)
